a1aea7c10a7ecfa3ab1c3ceab94022a6542de95e
Yordan <yordan.s.yordanov@outlook.com>
2017-10-06 12:12:12 -0400

    Initial commit

---
+++ TDAmeritrade.Client/Models/Interval.cs
@@ -0,0 +1,12 @@
+﻿// --------------------------------------------------------------------------------------------------------------------
+// <copyright file="Interval.cs" company="KriaSoft LLC">
+//   Copyright © 2013 Konstantin Tarkus, KriaSoft LLC. See LICENSE.txt
+// </copyright>
+// --------------------------------------------------------------------------------------------------------------------
+
+namespace TDAmeritrade.Client.Models
+{
+    public enum Interval
+    {
+    }
+}
---
+++ TDAmeritrade.Client/Models/UserExchangeStatus.cs
@@ -0,0 +1,15 @@
+﻿// --------------------------------------------------------------------------------------------------------------------
+// <copyright file="UserExchangeStatus.cs" company="KriaSoft LLC">
+//   Copyright © 2013 Konstantin Tarkus, KriaSoft LLC. See LICENSE.txt
+// </copyright>
+// --------------------------------------------------------------------------------------------------------------------
+
+namespace TDAmeritrade.Client.Models
+{
+    public enum UserExchangeStatus
+    {
+        Unknown,
+        Professional,
+        NonProfessional
+    }
+}
