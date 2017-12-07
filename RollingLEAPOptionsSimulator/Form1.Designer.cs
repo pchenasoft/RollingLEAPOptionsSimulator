f86bd0d0cc13e2352a798e46390a72744535c679
Yordan <yordan.s.yordanov@outlook.com>
2017-12-07 15:35:32 -0500

    aded icon

--- RollingLEAPOptionsSimulator/Form1.Designer.cs
+++ RollingLEAPOptionsSimulator/Form1.Designer.cs
@@ -28,6 +28,7 @@
         /// </summary>
         private void InitializeComponent()
         {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
             this.button2 = new System.Windows.Forms.Button();
             this.output = new System.Windows.Forms.TextBox();
             this.button3 = new System.Windows.Forms.Button();
@@ -92,6 +93,7 @@
             this.Controls.Add(this.button3);
             this.Controls.Add(this.output);
             this.Controls.Add(this.button2);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
             this.Name = "Form1";
             this.Text = "Form1";
             this.FormClosed += new System.Windows.Forms.FormClosedEventHandler(this.Form1_FormClosed);
